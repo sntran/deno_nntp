4b1cb90986f6c32ae15afb7a72093ab06c39207b
Son Tran-Nguyen <github@sntran.com>
2022-06-08 18:34:55 -0500

    refactor(examples/http):
    - Each request starts a NNTP connection.
    - Use Basic Authentication for login into news server.
    - Returns body in response body and headers in headers.
    - Use cookie to handle currently selected group and article.
    - Support posting using request's headers and body.
    - `head` search param to responds with headers in body.
    - `include` search param to include headers in response body.
    - Examples with `curl`.

--- examples/http.ts
+++ examples/http.ts
@@ -1,11 +1,41 @@
-// This is an example http server proxying NNTP.
+#!/usr/bin/env -S deno run --allow-net --allow-env
 
-/// <reference no-default-lib="true"/>
-/// <reference lib="deno.ns" />
-/// <reference lib="deno.worker" />
-import { serve, ConnInfo } from "https://deno.land/std@0.136.0/http/server.ts";
-import { router } from "https://crux.land/router@0.0.11";
-import { Client, Command } from "../mod.ts";
+/**
+ * An example http server proxying NNTP.
+ *
+ * The server listens on port defined in `PORT` environment variable, or 8000.
+ *
+ * The server handles routes in pattern `/:command/:args` and forwards requests
+ * to NNTP server with corresponding command.
+ *
+ * Authentication to NNTP server is done through HTTP Basic Authentication.
+ *
+ * To post, send the multi-line block data to `/post` or `/ihave/:msg-id`,
+ * with any request headers needed.
+ *
+ * ## Examples
+ *
+ * ```shell
+ * # Retrieves an article body by its message-id into a file.
+ * curl "http://localhost:8000/body/message-id@nntp-server" > article.txt
+ *
+ * # Posts a new article using data from a file.
+ * curl "http://localhost:8080/post" \
+ *   --header "Message-ID: <abc@def>" \
+ *   -H "From: poster@example.com" \
+ *   --data-binary "@article.txt"
+ * ```
+ *
+ * Note: When sending data with `curl`, use `--data-binary` instead of `--data`
+ * to preserve linebreaks, which are essential for parsing.
+ */
+import { ConnInfo, serve } from "https://deno.land/std@0.142.0/http/server.ts";
+import {
+  getCookies,
+  setCookie,
+} from "https://deno.land/std@0.142.0/http/cookie.ts";
+import { router } from "https://crux.land/router@0.0.12";
+import { Article, Client } from "../mod.ts";
 
 const {
   PORT = 8000,
@@ -26,121 +56,147 @@ const {
  * console.assert("".split("/").filter(identity).length === 0);
  * ```
  */
-function identity<T>(x: T): T { return x; }
-
-// Make a TCP connection to NNTP server
-const client = new Client({
-  port: parseInt(NNTP_PORT),
-  hostname: NNTP_HOSTNAME,
-  logLevel: "DEBUG",
-});
-
-const response = await client.connect();
-
-//#region Server
-console.log(`Listening on http://localhost:${ PORT }`);
-await serve(router(
-  {
-    "/": connect,
-    "/favicon.ico": () => new Response(null, {status: 404}),
-    "/login": login,
-    "POST@/authinfo": async (req: Request) => authinfo(req, await req.formData()),
-    "/:command/:args*": handle,
-    "/:command/": handle,
-  },
-), {
-  port: Number(PORT),
-});
-//#endregion
-
-function connect(_req: Request) {
-  const { status, statusText, headers } = response;
-
-  return new Response(statusText, {
-    status,
-    statusText,
-    headers,
-  });
+function identity<T>(x: T): T {
+  return x;
 }
 
-function login(req: Request) {
-  const url = new URL(req.url);
-
-  const body = `
-    <form action="/authinfo" method="POST" enctype="multipart/form-data">
-      <label>
-        <span>Username: </span>
-        <input name="USER" type="text" required />
-      </label>
-      <label>
-        <span>Password: </span>
-        <input name="PASS" type="password" autocomplete="current-password" required />
-      </label>
-
-      <input type="hidden" name="redirect_uri" value="${ url.searchParams.get("redirect_uri") || "" }" />
-
-      <button type="submit">Login</button>
-    </form>
-    `;
-
-  return new Response(body, {
-    headers: {
-      "content-type": "text/html;charset=utf-8",
-    },
-  });
-}
-
-async function authinfo(req: Request, formData: FormData, type = "USER"): Promise<Response> {
-  const url = new URL(req.url);
-
-  const value: string = formData.get(type) as string;
-  let response = await client.request(`AUTHINFO ${ type }` as Command, value);
-  switch (response.status) {
-    case 381:
-      response = await authinfo(req, formData, "PASS");
-      break;
-    case 481:
-    case 482:
-      response = Response.redirect(`${ url.origin }/login?redirect_uri=${ formData.get("redirect_uri") }`);
-      break;
-    default:
-      response = Response.redirect(`${ formData.get("redirect_uri") }`);
-      break;
+async function handle(
+  request: Request,
+  _c: ConnInfo,
+  { command, args = "" }: Record<string, string>,
+) {
+  //#region Authorization.
+  const authorization = request.headers.get("Authorization");
+  if (!authorization) {
+    return new Response("401 Unauthorized", {
+      status: 401,
+      statusText: "Unauthorized",
+      headers: {
+        "WWW-Authenticate": `Basic realm="Provider Login", charset="UTF-8"`,
+      },
+    });
   }
 
-  return response;
-}
+  const [, base64 = ""] = authorization.match(/^Basic\s+(.*)$/) || [];
+  const [username, password] = atob(base64).split(":");
 
-async function handle(req: Request, _conn: ConnInfo, { command, args = "" }: Record<string, string>) {
-  const url = new URL(req.url);
-  const {
-    headers: originalHeaders,
-    status,
-    statusText,
-    body,
-  } = await client.request(
-    command as Command,
-    ...decodeURIComponent(args).split("/").filter(identity),
-  );
+  const client = await await Client.connect({
+    hostname: NNTP_HOSTNAME,
+    port: Number(NNTP_PORT),
+    ssl: true,
+    logLevel: "DEBUG",
+  });
+  // Authenticates with provider.
+  await client.authinfo(username, password);
+  //#endregion Authorization
 
-  // The NNTP server can respond to a client command with a a 480 response
-  // to indicate that the client MUST authenticate and/or authorize in order
-  // to use that command or access the indicated resource.
-  if (status === 480) {
-    return Response.redirect(`${ url.origin }/login?redirect_uri=${ url }`);
+  // Checks if any group or article was selected before.
+  const { group, article } = getCookies(request.headers);
+  if (group) {
+    await client.group(group);
+  }
+  if (article) {
+    await client.stat(article);
   }
 
-  const headers = new Headers(originalHeaders);
+  const params = decodeURIComponent(args).split("/").filter(identity);
+
+  let response: Response = new Response();
+  if (request.body) {
+    const article = new Article(request);
+    // Cleans up HTTP headers.
+    article.headers.delete("Accept");
+    article.headers.delete("Authorization");
+    article.headers.delete("Content-Length");
+    article.headers.delete("Content-Type");
+    article.headers.delete("Host");
+    article.headers.delete("User-Agent");
+
+    if (command === "post") {
+      response = await client.post(article);
+    } else if (command === "ihave") {
+      response = await client.ihave(params[0], article);
+    }
+  } else {
+    response = await client.request(command, ...params);
+  }
+
+  const { status, statusText, headers: articleHeaders } = response;
+
+  const headers = new Headers(articleHeaders);
   headers.append("content-type", "text/plain;charset=utf-8");
   headers.append("X-Content-Type-Options", "nosniff");
 
-  // Needs to create a new `Response` here to include the special header
-  // `X-Content-Type-Options: nosniff` to prevent browser from "sniffing"
-  // the body content and deciding to popup for download instead.
-  return new Response(body, {
+  // Stores selected group or article to cookie for retrieval later.
+  switch (status) {
+    case 211: // GROUP and LISTGROUP
+      setCookie(headers, {
+        name: "group",
+        value: params[0],
+        path: "/",
+      });
+      break;
+    case 220: // ARTICLE
+    case 221: // HEAD
+    case 222: // BODY
+    case 223: { // LAST, NEXT, STAT
+      const [, n, _messageId] = statusText.match(/([\d]+)\s(<[\x21-\x7E]+>)/) ||
+        [];
+      setCookie(headers, {
+        name: "article",
+        value: n,
+        path: "/",
+      });
+      break;
+    }
+  }
+
+  const responseInit = {
     // NNTP can have status below 200, but HTTP can't, so we normalize.
     status: status < 200 ? 200 : status,
     statusText,
     headers,
+  };
+
+  const { searchParams } = new URL(request.url);
+  // Responds with just the body if `head` or `include` flag is not requested.
+  if (!searchParams.has("head") && !searchParams.has("include")) {
+    return new Response(response.body, responseInit);
+  }
+
+  let body = "";
+
+  articleHeaders.forEach((value: string, name: string) => {
+    body += `${name}: ${value}\r\n`;
   });
+
+  if (searchParams.has("include")) {
+    // If `body` has content, we have some articleHeaders.
+    if (body) {
+      body += "\r\n";
+    }
+
+    body += await response.text();
+  }
+
+  // // Prepends with response code and response text.
+  body = `${status} ${statusText}` + (body ? `\r\n${body}` : "");
+
+  return new Response(body, responseInit);
 }
+
+//#region Server
+await serve(
+  router(
+    {
+      "/": () => new Response("Hello NNTP"),
+      "/favicon.ico": () => new Response(null, { status: 404 }),
+      "/:command/:args*": handle,
+      "/:command/": handle,
+    },
+  ),
+  {
+    port: Number(PORT),
+  },
+);
+//#endregion
